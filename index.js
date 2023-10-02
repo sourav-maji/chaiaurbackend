d7620776c643d86c977d6c60609bfc1a0da7fb34
sourav-maji <s722143@gmail.com>
2023-10-02 23:08:17 +0530

    second commit

--- index.js
+++ index.js
@@ -4,6 +4,41 @@ require('dotenv').config()
 
 const express = require("express")
 const app = express()
+const gitHunData = {
+    "login": "sourav-maji",
+    "id": 68387255,
+    "node_id": "MDQ6VXNlcjY4Mzg3MjU1",
+    "avatar_url": "https://avatars.githubusercontent.com/u/68387255?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/sourav-maji",
+    "html_url": "https://github.com/sourav-maji",
+    "followers_url": "https://api.github.com/users/sourav-maji/followers",
+    "following_url": "https://api.github.com/users/sourav-maji/following{/other_user}",
+    "gists_url": "https://api.github.com/users/sourav-maji/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/sourav-maji/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/sourav-maji/subscriptions",
+    "organizations_url": "https://api.github.com/users/sourav-maji/orgs",
+    "repos_url": "https://api.github.com/users/sourav-maji/repos",
+    "events_url": "https://api.github.com/users/sourav-maji/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/sourav-maji/received_events",
+    "type": "User",
+    "site_admin": false,
+    "name": "Sourav Maji",
+    "company": null,
+    "blog": "",
+    "location": "India",
+    "email": null,
+    "hireable": null,
+    "bio": null,
+    "twitter_username": null,
+    "public_repos": 14,
+    "public_gists": 0,
+    "followers": 0,
+    "following": 1,
+    "created_at": "2020-07-16T14:29:15Z",
+    "updated_at": "2023-09-19T08:22:24Z"
+  }
+
 
 app.get("/", (req,res)=>{
     res.send("Hello World!!")
@@ -19,6 +54,10 @@ app.get("/login" , (req,res) =>{
 
 app.get("/youtube" , (req,res)=>{
     res.send("<h2>Chai aur code</h2>")
+})
+
+app.get("/github" , (req,res)=>{
+    res.json(gitHunData)
 })
 
 app.listen(process.env.PORT,()=>{
